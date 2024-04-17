2327e6c11e2202ee6affaa953185c8da5ca15a3c
euniiiii <hee9920@naver.com>
2024-04-17 14:24:48 +0900

    프로필 토글 구현
    프로필 클릭 시 메뉴바 토글 기능 추가함

--- js/edit-info.js
+++ js/edit-info.js
@@ -3,14 +3,22 @@ const modalCloseButton = document.querySelector('.cancel-btn');
 const modalCloseButton2 = document.querySelector('.check-btn');
 const modal = document.querySelector('.modal');
 
+const dropdownBtn = document.querySelector(".header-profile-img");
+const dropdownContent = document.querySelector(".dropdown-content");
+
 modalOpenButton.addEventListener('click', () => {
-modal.classList.remove('hidden');
+    modal.classList.remove('hidden');
 });
 
 modalCloseButton.addEventListener('click', () => {
-modal.classList.add('hidden');
+    modal.classList.add('hidden');
 });
 
 modalCloseButton2.addEventListener('click', () => {
     modal.classList.add('hidden');
 });
+
+dropdownBtn.addEventListener('click', () => {
+    dropdownContent.classList.toggle('active');
+    console.log("실행완료");
+})
