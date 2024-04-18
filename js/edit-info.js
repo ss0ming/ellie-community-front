87a8cf68a89f672caccb73ce8da41c803dd8e0d7
euniiiii <hee9920@naver.com>
2024-04-18 13:30:43 +0900

    회원정보 수정 페이지 토스트 기능 구현
    닉네임 유효성 검사 기능 구현
    수정 완료 버튼 클릭 후 유효성 검사 통과하면 토스트 메시지 보여주는 기능 구현

--- js/edit-info.js
+++ js/edit-info.js
@@ -6,6 +6,12 @@ const modal = document.querySelector('.modal');
 const dropdownBtn = document.querySelector(".header-profile-img");
 const dropdownContent = document.querySelector(".dropdown-content");
 
+const modifyBtn = document.getElementById('modify-btn');
+const nickname = document.getElementById('nickname-input');
+const nicknameHelper = document.getElementById('nickname-helper');
+const toastMessage = document.getElementById('toast-message');
+const blankPattern = /[\s]/g;
+
 modalOpenButton.addEventListener('click', () => {
     modal.classList.remove('hidden');
 });
@@ -22,3 +28,35 @@ dropdownBtn.addEventListener('click', () => {
     dropdownContent.classList.toggle('active');
     console.log("실행완료");
 })
+
+// 수정 완료 버튼 클릭 시 토스트 생성
+modifyBtn.addEventListener('click', () => {
+    if (nicknameValidCheck()) {
+        toastOn();
+    } 
+})
+
+function toastOn() {
+    toastMessage.classList.add('active');
+    setTimeout(function() {
+        toastMessage.classList.remove('active');
+    }, 1000) ;
+}
+
+function nicknameValidCheck() {
+    if (!nickname.value) {
+        nicknameHelper.innerHTML = '* 닉네임을 입력해주세요.';
+        return false;
+    } else if (blankPattern.test(nickname.value) == true) {
+        console.log("1")
+        nicknameHelper.innerHTML = '* 띄어쓰기를 없애주세요.';
+        return false;
+    } else if (nickname.value.length > 10) {
+        console.log("2")
+        nicknameHelper.innerHTML = '* 닉네임은 최대 10자까지 가능합니다.';
+        return false;
+    }
+
+    nicknameHelper.innerHTML = '';
+    return true;
+}
